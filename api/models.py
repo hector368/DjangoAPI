04948f611b035c9c96dba576ceb3081a02cd2007
hector368 <2021451031@teschi.edu.mx>
2024-10-19 16:44:21 -0600

    Primer Comentario

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class generos(models.Model):
+    genero_id = models.AutoField(primary_key=True)
+    nombre_genero = models.CharField(max_length=255)
+    
+    class Meta:
+        db_table = "generos"
+
+class usuarios(models.Model):
+    usuario_id = models.AutoField(primary_key=True)
+    nombre_usuario = models.CharField(max_length=255)
+    fk_generos = models.ForeignKey(generos, on_delete=models.CASCADE,default=0)
+    
+    class Meta:
+        db_table = "usuarios"
+
