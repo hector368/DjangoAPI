67b3c103b20960f059bf3547b6e75e556ace533a
hector368 <2021451031@teschi.edu.mx>
2024-10-20 18:23:46 -0600

    Login parte 1

--- DjangoApi/urls.py
+++ DjangoApi/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.login.login_view import login_view
+from api.login.login_view import (
+    login_view, register_view, forgot_view,
+    
+    )
 from api.home.home_view import home_view
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('login/', login_view, name = "login_vista"),
+    path('login/', login_view, name = "login"),
+    path('register/', register_view, name = "register"),
+    path('forgot/', forgot_view, name = "forgot"),
     path('', home_view, name = "home"),
 ]
--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def login_view(request):
     template_view = "auth-login.html"
-    
+    return render (request,template_name=template_view)
+
+# View for register
+def register_view(request):
+    template_view = "auth-register.html"
+    return render (request,template_name=template_view)
+
+# View for forget
+def forgot_view(request):
+    template_view = "auth-forgot-password.html"
     return render (request,template_name=template_view)
